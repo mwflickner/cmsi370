1585b1949f42bed114830e751b08c63f3e932b1a
Matthew <mwflickner@gmail.com>
2014-12-03 18:32:56 -0800

    it works better now console logs movement etc

--- widget-from-scratch/jquery.dPad.js
+++ widget-from-scratch/jquery.dPad.js
@@ -1,47 +1,73 @@
 //(function ($){
-    
-   /* setUpDPad: function (jQueryElements){
-        jQueryElements
-            .addClass("dPad")
-            
-    },*/
+    var dPad = {
 
-
-   /* $.fn.dPad = function(){
         
-        return this;
+        
+        setUpDPad: function (jQueryElements){
+            jQueryElements
+                .addClass("dPad")
+                .keydown(this.keyer)
+        },
+
+        keyer: function (event){
+            switch(event.which){
+                case 37: // left arrow
+                    console.log("SWITCH WORKS");
+                    dPad.goLeft();
+                    break;
+
+                case 38: // up arrow
+                    console.log("SWITCH WORKS!");
+                    dPad.goUp();
+                    break;
+
+                case 39: // right arrow
+                    console.log("SWITCH WORKS");
+                    dPad.goRight();
+                    break;
+
+                case 40: // down arrow
+                    console.log("SWITCH WORKS");
+                    dPad.goDown();
+                    break;  
+            }
+        },
+
+
+
+        goLeft: function (){
+            console.log("LEFT!");
+        },
+
+        goUp: function (){
+            console.log("UP!");
+        },
+
+        goRight: function (){
+            console.log("RIGHT!");
+        },
+
+        goDown: function (){
+            console.log("DOWN!");
+        },
+
+        highlight: function (){
+            $()
+        }
+
+
+
+    
+
+        
+           
+        
+
+    };//closes var dPad
+
+/*    $.fn.dPad = function () {
+        dPad.setUpDPad(this);
     };
 
-    */
-
-    var dPad =
-
-        /*getKeyInput:*/ function(){
-            $(document).keydown(function(event){
-                switch(event.which){
-                    case 37: // left arrow
-                        console.log("LEFT!!");
-                        $("#test").css('left', $("#test").offset().left - 5);
-                        break;
-
-                    case 38: // up arrow
-                        console.log("UP!");
-                        $("#test").css('top', $("#test").offset().top -5);
-                        break;
-
-                    case 39: // right arrow
-                        console.log("RIGHT");
-                        $("#test").css('left', $("#test").offset().left +5);
-                        break;
-
-                    case 40: // down arrow
-                        console.log("DOWN!");
-                        $("#test").css('top', $("#test").offset().top +5);
-                        break;  
-                }
-            });
-        }
-    //};closes var dPad
-
-//}(jQuery));
+}(jQuery)); */
     
